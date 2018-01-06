8921f3d5aa96fb09b572a3f4438e95e642a65d91
Oleg Mykhailov <o.a.mykhailov@gmail.com>
2018-01-07 01:00:58 +0200

    S10l106:Using a Service for Cross-Component Communication

--- ms-app/src/app/recipes/recipes-list/recipes-list.component.ts
+++ ms-app/src/app/recipes/recipes-list/recipes-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
 import { Recipe } from '../recipe.model';
 import { RecipeService } from '../recipe.service';
 
@@ -8,7 +9,6 @@ import { RecipeService } from '../recipe.service';
   styleUrls: ['./recipes-list.component.css']
 })
 export class RecipesListComponent implements OnInit {
-  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[];
 
   constructor(private recipeService: RecipeService) { }
@@ -16,9 +16,4 @@ export class RecipesListComponent implements OnInit {
   ngOnInit() {
     this.recipes = this.recipeService.getRecipe();
   }
-
-  onRecipeSelected(recipe: Recipe) {
-    this.recipeWasSelected.emit(recipe);
-  }
-
 }
