882939ac5d7f33f9a4dff2d77d427e814430dc49
Oleg Mykhailov <o.a.mykhailov@gmail.com>
2018-01-07 01:33:06 +0200

    s10l108: Using Services for 'Push Notifications'

--- ms-app/src/app/shopping-list/shopping-list.component.ts
+++ ms-app/src/app/shopping-list/shopping-list.component.ts
@@ -9,11 +9,17 @@ import { ShoppingListService } from './shopping-list.service';
   styleUrls: ['./shopping-list.component.css'],
 })
 export class ShoppingListComponent implements OnInit {
-  ingredients: Ingredient[] = [];
+  ingredients: Ingredient[];
 
   constructor(private shoppingListService: ShoppingListService) { }
 
   ngOnInit() {
     this.ingredients = this.shoppingListService.getIngredients();
+    this.shoppingListService.ingredientsChanged
+      .subscribe(
+        (ingredients: Ingredient[]) => {
+          this.ingredients = ingredients;
+        }
+      );
   }
 }
