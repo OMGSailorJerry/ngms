42505dd40071d70bd07ba76f82cbbe5986223ed2
Oleg Mykhailov <o.a.mykhailov@gmail.com>
2018-01-06 23:39:13 +0200

    s10l103: Managing Recipes in a Recipe Service

---
+++ ms-app/src/app/recipes/recipe.service.ts
@@ -0,0 +1,19 @@
+import { Recipe } from './recipe.model';
+
+export class RecipeService {
+  private recipes: Recipe[] = [
+    new Recipe(
+      'Test Recipe',
+      'This is test',
+      'http://maxpixel.freegreatpicture.com/static/photo/640/Melon-Meat-Fruit-Ham-Food-625130.jpg'),
+    new Recipe(
+      'Another test Recipe',
+      'This is another test',
+      'http://maxpixel.freegreatpicture.com/static/photo/640/Melon-Meat-Fruit-Ham-Food-625130.jpg')
+  ];
+
+  getRecipe() {
+    console.log(this.recipes);
+    return this.recipes.slice();
+  }
+}
