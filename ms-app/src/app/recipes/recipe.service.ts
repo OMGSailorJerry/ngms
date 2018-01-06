4549ae801b374009c86659d58743ce671c137a11
Oleg Mykhailov <o.a.mykhailov@gmail.com>
2018-01-07 01:40:37 +0200

    s10l109: Adding Ingredients to Recipes

--- ms-app/src/app/recipes/recipe.service.ts
+++ ms-app/src/app/recipes/recipe.service.ts
@@ -1,6 +1,7 @@
 import { EventEmitter } from '@angular/core';
 
 import { Recipe } from './recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
 
 export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
@@ -9,11 +10,19 @@ export class RecipeService {
     new Recipe(
       'Test Recipe',
       'This is test',
-      'http://maxpixel.freegreatpicture.com/static/photo/640/Melon-Meat-Fruit-Ham-Food-625130.jpg'),
+      'http://maxpixel.freegreatpicture.com/static/photo/640/Melon-Meat-Fruit-Ham-Food-625130.jpg',
+      [
+        new Ingredient('Meat', 1),
+        new Ingredient('French Fries', 20)
+      ]),
     new Recipe(
       'Another test Recipe',
       'This is another test',
-      'http://maxpixel.freegreatpicture.com/static/photo/640/Melon-Meat-Fruit-Ham-Food-625130.jpg')
+      'http://maxpixel.freegreatpicture.com/static/photo/640/Melon-Meat-Fruit-Ham-Food-625130.jpg',
+      [
+        new Ingredient('Meat', 1),
+        new Ingredient('Bans', 2)
+      ])
   ];
 
   getRecipe() {
