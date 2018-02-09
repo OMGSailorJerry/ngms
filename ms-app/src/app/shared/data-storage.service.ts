7ed5d077ba786f2c176c0fbaff177d52fa6197e0
Oleg Mikhailov <omikhailov@ace-st.com>
2018-02-09 16:46:07 +0200

    s23l290: HttpClient Unlock

--- ms-app/src/app/shared/data-storage.service.ts
+++ ms-app/src/app/shared/data-storage.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Http, Response } from '@angular/http';
+import { HttpClient } from '@angular/common/http';
 import 'rxjs/Rx';
 
 import { RecipeService } from '../recipes/recipe.service';
@@ -9,14 +9,14 @@ import { AuthService } from '../auth/auth.service';
 
 @Injectable()
 export class DataStorageService {
-  constructor(private http: Http,
+  constructor(private httpClient: HttpClient,
               private recipeService: RecipeService,
               private authService: AuthService) {}
 
   storeRecipes() {
     const token = this.authService.getToken();
 
-    return this.http.put(
+    return this.httpClient.put(
       'https://ng-recipe-book-f49b2.firebaseio.com/recipes.json?auth=' + token,
       this.recipeService.getRecipe()
     );
@@ -25,11 +25,10 @@ export class DataStorageService {
   getRecipes() {
     const token = this.authService.getToken();
 
-    this.http.get('https://ng-recipe-book-f49b2.firebaseio.com/recipes.json?auth=' + token)
+    this.httpClient.get<Recipe[]>('https://ng-recipe-book-f49b2.firebaseio.com/recipes.json?auth=' + token)
       .map(
-          (response: Response) => {
-            const recipes: Recipe[] = response.json();
-            for (const recipe of recipes) {
+          (recipes) => {
+            for (let recipe of recipes) {
               if (!recipe['ingredients']) {
                 recipe['ingredients'] = [];
               }
