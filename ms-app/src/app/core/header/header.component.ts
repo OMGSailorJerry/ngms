9b84fd1b1db7dd1a607d16f7f878713d7fa01e19
Oleg Mikhailov <omikhailov@ace-st.com>
2018-02-11 23:38:02 +0200

    s24l320: Using Authentication

--- ms-app/src/app/core/header/header.component.ts
+++ ms-app/src/app/core/header/header.component.ts
@@ -1,20 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs/Observable';
 // import { HttpEvent, HttpEventType } from '@angular/common/http';
 
 import { DataStorageService } from '../../shared/data-storage.service';
 import { RecipeService } from '../../recipes/recipe.service';
 import { AuthService } from '../../auth/auth.service';
+import * as fromApp from '../../store/app.reducer';
+import * as fromAuth from '../../auth/store/auth.reducers';
 
 @Component({
   selector: 'app-header',
   templateUrl: 'header.component.html'
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  authState: Observable<fromAuth.State>;
+
   constructor(
     private dataStorageService: DataStorageService,
-    private recipeService: RecipeService,
-    public authService: AuthService
+    private authService: AuthService,
+    private store: Store<fromApp.AppState>
   ) {}
+
+  ngOnInit() {
+    this.authState = this.store.select('authState');
+  }
 
   onSaveData() {
     this.dataStorageService.storeRecipes()
